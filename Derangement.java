33a02460f3f5bb3c5914eb84bfaeac8a4748879c
Jon2147483647 <37780414+ccevik48@users.noreply.github.com>
2019-03-29 10:26:48 -0400

    Updated with comments
    added documentation to code to understand better

--- Derangement.java
+++ Derangement.java
@@ -13,24 +13,28 @@ import java.io.*;
  * @author Can
  */
 public class Derangement {
-
+    /**
+     * 
+     * @param n : number of objects being considered
+     * @return  : an array which is a derangement of an array of size 'n'
+     */
     public static int[] derange(int n) {
         int[] arr = new int[n];
-        Queue<Integer> q = new LinkedList();
-        Random r = new Random();
+        Queue<Integer> q = new LinkedList(); //use a queue to ensure every number from 1 to 'n' is used once
+        Random r = new Random();             //used to randomly index the current number
         for (int i = 0; i < n; i++) {
             q.add(i);
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {        //initialize array elements with -1
             arr[i] = -1;
         }
-        int tmp = 0;
+        int tmp = 0;                         //temporary variable to help with indexing
         while (!q.isEmpty()) {
             do {
-                tmp = r.nextInt(n);
+                tmp = r.nextInt(n);          //keep generating a random number until a usable index is found
             } while (tmp == q.peek() && (arr[tmp] != -1));
             if (arr[tmp] > -1 || tmp == q.peek()) {
-
+                    //do nothing if index is the same as tmp or is taken by another number
             } else {
                 arr[tmp] = q.remove();
             }
@@ -40,6 +44,7 @@ public class Derangement {
         }
 
         return arr;
+        //IGNORE THIS BIT OF CODE
         /*Random rand = new Random();
         int[] result = new int[n];
         SortedMap<Double, Integer> map = new TreeMap<>();
@@ -59,7 +64,7 @@ public class Derangement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(derange(7)));
+        System.out.println(Arrays.toString(derange(7))); //change to however many n is needed
     }
 
 }
